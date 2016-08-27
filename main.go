d08fde64567edf6a13b629ceab9b4cd5d48eca2b
S.S <ss@ss.com>
2016-08-28 08:30:59 +0900

    file copy.

--- main.go
+++ main.go
@@ -3,45 +3,76 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
-func readConfig() {
-	fmt.Println("read config start.")
-
-	var fp *os.File
-	fp, err := os.OpenFile("config.txt", os.O_RDONLY, 0)
+func handleErr(err error) {
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+func ReadConfig() (m map[string]string) {
+	var fp *os.File
+	fp, err := os.OpenFile("config.txt", os.O_RDONLY, 0)
+	handleErr(err)
 
 	defer func() {
 		fp.Close()
 	}()
 
+	m = map[string]string{}
 	scanner := bufio.NewScanner(fp)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		line := scanner.Text()
+		kv := strings.Split(line, "=")
+		v, ok := m[kv[0]]
+		if ok {
+			t := v
+			m[kv[0]] = t + "$" + kv[1]
+		} else {
+			m[kv[0]] = kv[1]
+		}
 	}
-	if err := scanner.Err(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	handleErr(scanner.Err())
+
+	return m
+}
+
+func CopyToEachDir(m map[string]string) {
+	fromDir := m["fromDir"]
+	fromFile := m["fromFile"]
+	from := fromDir + fromFile
+	toFiles := m["to"]
+	fmt.Println(toFiles)
+	toArray := strings.Split(toFiles, "$")
+
+	for _, to := range toArray {
+		src, err := os.Open(from)
+		handleErr(err)
+		defer src.Close()
+
+		dst, err := os.Create(to)
+		handleErr(err)
+		defer dst.Close()
+
+		_, err = io.Copy(dst, src)
+		handleErr(err)
 	}
 }
 
 func main() {
 	http.HandleFunc("/fs/start", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Start!")
-		readConfig()
-		fmt.Println("after readConfig")
+		m := ReadConfig()
+		fmt.Println(m)
+		CopyToEachDir(m)
 	})
 
 	err := http.ListenAndServe(":7109", nil)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
+	handleErr(err)
 }
