ae6e63648d99944ea1e2c42900f49ca7a6f1e240
S.S <ss@ss.com>
2016-10-18 07:37:07 +0900

    TestMain

--- copy_test.go
+++ copy_test.go
@@ -7,6 +7,22 @@ import (
 
 const copySpec string = "[仕様] 任意のパスにあるファイルが任意のパスにコピーされる"
 
+func TestMain(m *testing.M) {
+	setUp()
+	exitCode := m.Run()
+	shutdown()
+	os.Exit(exitCode)
+}
+
+func setUp() {
+	// とりあえず何も無し
+}
+
+func shutdown() {
+	// TestCopyToEachDir()に記述していたものを移行
+	os.Remove("testdata/copyTo.txt")
+}
+
 func TestCopyToEachDir(t *testing.T) {
 	t.Log(copySpec)
 	copyTos := []CopyTo{CopyTo{ToDir: "testdata/", ToFile: "copyTo.txt"}}
@@ -16,7 +32,7 @@ func TestCopyToEachDir(t *testing.T) {
 		t.Fatalf("コピー時にエラー発生 %s", copier.err)
 	}
 	// [MEMO] 後始末用のメソッドを定義する機能はないのかな。
-	os.Remove("testdata/copyTo.txt")
+	// os.Remove("testdata/copyTo.txt")
 }
 
 func BenchmarkCopyToEachDir(b *testing.B) {
